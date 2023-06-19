bcab1c47682aaa2b7059fe09ad85b8af4b4d60e7
Magdalena-23 <m.andonovska99@gmail.com>
2023-06-19 13:46:01 +0200

    forgot and reset password, validation and axios

--- client/src/components/auth/ForgotPassword/ForgotPassword.js
+++ client/src/components/auth/ForgotPassword/ForgotPassword.js
@@ -1,16 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import classes from "./ForgotPassword.module.css";
 import FormWrapper from "../../common/FormWrapper";
 import Input from "../../common/Input";
 import Button from "../../common/Button/Button";
+import axios from "../../../api/axios";
+
+const FORGOT_PASSWORD_URL = "api/auth/forgot-password";
 
 function ForgotPassword() {
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+  const [msg, setMsg] = useState("");
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+    setEmailError("");
+    setErrorMsg("");
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const emailPattern = /\S+@\S+\.\S+/;
+    if (email.trim().length === 0) {
+      setEmailError("Email is required");
+      return;
+    } else if (!emailPattern.test(email)) {
+      setEmailError("Invalid email");
+      return;
+    }
+
+    try {
+      await axios.post(FORGOT_PASSWORD_URL, JSON.stringify({ email }), {
+        headers: { "Content-Type": "application/json" },
+        withCredentials: true,
+      });
+      setMsg("Email sent! Tap the link in that email to reset your password.");
+    } catch (err) {
+      console.log(err);
+      if (!err?.response) {
+        setErrorMsg("No Server Response");
+      } else if (err.response.status === 404) {
+        setEmailError(err.response.data);
+      } else {
+        setErrorMsg("Failed. Try Again");
+      }
+    }
+  };
   return (
-    <FormWrapper>
+    <FormWrapper onSubmit={handleSubmit}>
       <h1>Forgot Password</h1>
+      {errorMsg && <span className={classes.error}>{errorMsg}</span>}
+      {msg && <span className={classes.msg}>{msg}</span>}
       <div className={classes.spacing}>
-        <Input type="email" id="email" label="Email" />
+        <Input
+          type="email"
+          id="email"
+          label="Email"
+          value={email}
+          onChange={handleEmailChange}
+          error={emailError}
+        />
         <Button type="form">Send password reset email</Button>
         <Link to="/login">
           <Button type="link">Back to login</Button>
--- client/src/components/auth/ResetPassword/ResetPassword.js
+++ client/src/components/auth/ResetPassword/ResetPassword.js
@@ -1,16 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import classes from "./ResetPassword.module.css";
 import FormWrapper from "../../common/FormWrapper";
 import Input from "../../common/Input";
 import Button from "../../common/Button/Button";
+import axios from "../../../api/axios";
+
+const RESET_URL = "/api/auth/reset-password";
 
 function ResetPassword() {
+  const [password, setPassword] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [confirmPasswordError, setConfirmPasswordError] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+    setPasswordError("");
+    setConfirmPasswordError("");
+    setErrorMsg("");
+  };
+
+  const handleConfirmPasswordChange = (e) => {
+    setConfirmPassword(e.target.value);
+    setConfirmPasswordError("");
+    setPasswordError("");
+    setErrorMsg("");
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!password) {
+      setPasswordError("Password is required");
+      return;
+    } else if (password.trim().length < 6) {
+      setPasswordError("Password must be at least 6 characters");
+      return;
+    } else if (!confirmPassword) {
+      setConfirmPasswordError("Confirm Password is required");
+      return;
+    } else if (confirmPassword !== password) {
+      setConfirmPasswordError("Passwords do not match");
+      return;
+    }
+
+    try {
+      const token = window.location.pathname.split("/").pop();
+      await axios.post(
+        RESET_URL,
+        JSON.stringify({
+          newPassword: password,
+          confirm_password: confirmPassword,
+          token,
+        }),
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        }
+      );
+      window.location.href = "/login";
+    } catch (err) {
+      console.log(err);
+      if (!err?.response) {
+        setErrorMsg("No Server Response");
+      } else if (err.response.status === 401) {
+        setErrorMsg(err.response.data.message);
+      } else {
+        setErrorMsg("Reset Password Failed. Try Again.");
+      }
+    }
+  };
   return (
-    <FormWrapper>
+    <FormWrapper onSubmit={handleSubmit}>
       <h1>Reset Password</h1>
-      <Input type="password" id="password" label="Password" />
-      <Input type="password" id="confirm-password" label="Re-type password" />
+      {errorMsg && <span className={classes.error}>{errorMsg}</span>}
+      <Input
+        type="password"
+        id="password"
+        label="Password"
+        onChange={handlePasswordChange}
+        value={password}
+        error={passwordError}
+      />
+      <Input
+        type="password"
+        id="confirm-password"
+        label="Re-type password"
+        onChange={handleConfirmPasswordChange}
+        value={confirmPassword}
+        error={confirmPasswordError}
+      />
       <div className={classes.spacing}>
         <Button type="form">Reset Password</Button>
         <Link to="/login">
