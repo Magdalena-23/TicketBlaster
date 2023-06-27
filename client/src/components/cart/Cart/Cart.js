14d0bbb8bef3c56e29195ae15d6ec7af7856d8a7
Magdalena-23 <m.andonovska99@gmail.com>
2023-06-27 15:20:31 +0200

    back navigation added

--- client/src/components/cart/Cart/Cart.js
+++ client/src/components/cart/Cart/Cart.js
@@ -1,17 +1,26 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import classes from "./Cart.module.css";
 import Button from "../../common/Button/Button";
 import CartItem from "../CartItem/CartItem";
 import Title from "../../common/Title/Title";
 
 const Cart = () => {
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
+
   return (
     <div>
       <Title>Shopping Cart</Title>
       <CartItem />
       <CartItem />
       <div className={classes["btn-container"]}>
-        <Button className={classes.btn}>Back</Button>
+        <Button className={classes.btn} onClick={goBack}>
+          Back
+        </Button>
         <Button className={`${classes.btn} ${classes["btn-pink"]}`}>
           Check Out
         </Button>
